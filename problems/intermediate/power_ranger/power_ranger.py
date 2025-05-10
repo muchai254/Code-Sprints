fc9003c45a34547c65448b9ffb815f5e8b03d2b1
m-njoki <161509249+m-njoki@users.noreply.github.com>
2025-05-10 13:37:11 +0300

    Update power_ranger.py

--- problems/intermediate/power_ranger/power_ranger.py
+++ problems/intermediate/power_ranger/power_ranger.py
@@ -1,4 +1,20 @@
+def getPowerRange(n, a, b):
+    count = 0
+    x = 1
 
-def getPowerRange(n,a,b):
+    while True:
+        power = x ** n
+        if power > b:
+            break
+        if power >= a:
+            count += 1
+        x += 1
 
-    return 0
+    return count
+
+
+print(getPowerRange(2, 49, 65))  
+print(getPowerRange(3, 1, 27))     
+print(getPowerRange(10, 1, 5))     
+print(getPowerRange(5, 31, 33))    
+print(getPowerRange(4, 250, 1300)) 
