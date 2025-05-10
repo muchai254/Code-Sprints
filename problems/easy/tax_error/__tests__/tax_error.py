22dfe76c4db537c5a88dcbd90ce791623f4eb973
RaphaelKaira <113975698+Raphael-pix@users.noreply.github.com>
2025-05-10 09:52:03 +0300

    Adding python imports

--- problems/easy/tax_error/__tests__/tax_error.py
+++ problems/easy/tax_error/__tests__/tax_error.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from problems.easy.tax_error import calculate_tax
 
 testcase = [10000,0,-500]
