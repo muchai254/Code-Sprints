160277d40098e02c7ada8d26150ca45e03a9e953
m-njoki <161509249+m-njoki@users.noreply.github.com>
2025-05-10 13:17:28 +0300

    Update discount.java

--- problems/easy/discount/discount.java
+++ problems/easy/discount/discount.java
@@ -4,6 +4,10 @@ public class discount {
     public static double get_discount(float p,float d){
 
         // write you code here
-        return 0.000;
+        double finalPrice = p - (p * d/100);
+        return finalPrice;
     }
+}
+public static void main(String[] args){
+    System.out.println(get_discount(1500,50));
 }
