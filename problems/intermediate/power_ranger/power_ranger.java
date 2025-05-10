a97a875255623c46dc8a3c87e39792dead43b578
m-njoki <161509249+m-njoki@users.noreply.github.com>
2025-05-10 13:33:25 +0300

    Update power_ranger.java

--- problems/intermediate/power_ranger/power_ranger.java
+++ problems/intermediate/power_ranger/power_ranger.java
@@ -1,8 +1,28 @@
 package problems.intermediate.power_ranger;
 
 public class power_ranger {
-    public static int getPowerRange(int n,int a, int b){
-        // put youer code here
-        return 0;
+    public static int getPowerRange(int n, int a, int b) {
+        int count = 0;
+        int x = 1;
+
+        while (true) {
+            double power = Math.pow(x, n);
+            if (power > b) {
+                break;
+            }
+            if (power >= a) {
+                count++;
+            }
+            x++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPowerRange(2, 49, 65));
+        System.out.println(getPowerRange(3, 1, 27));
+        System.out.println(getPowerRange(10, 1, 5)); 
+        System.out.println(getPowerRange(5, 31, 33));
+        System.out.println(getPowerRange(4, 250, 1300)); 
     }
 }
