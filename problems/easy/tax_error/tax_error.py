75f67f7bde429dd9c6200ab51cf494048366aca1
muchai254 <63966340+muchai254@users.noreply.github.com>
2025-05-10 12:30:32 +0300

    Update tax_error.py

--- problems/easy/tax_error/tax_error.py
+++ problems/easy/tax_error/tax_error.py
@@ -1,9 +1,9 @@
 def calculate_tax(income):
-    if income < 0:
-        return "Income can't be negative" 
-    tax_rate = 0.15
-    tax = 0
-    income * tax_rate  
+    if income <= 0:
+        return 0
+    else:
+        tax_rate = 0.15
+        tax = income * tax_rate  
     return tax
 
 print(calculate_tax(10000)) 
