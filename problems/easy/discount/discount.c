b8a2d5c4636ccfda6f98ae2080730b9a10ec1c16
m-njoki <161509249+m-njoki@users.noreply.github.com>
2025-05-10 13:18:47 +0300

    Updated discount.c

--- problems/easy/discount/discount.c
+++ problems/easy/discount/discount.c
@@ -1,6 +1,20 @@
-
 float cal_discount(float price, float percentage){
-    // put your code here
+    float discount = (price * percentage) / 100;
+    float finalPrice = price - discount;
+    return finalPrice;
+}
 
-    return 0.0;
+int main(){
+    float price, percentage, finalPrice;
+
+    printf("Enter the price:\n ");
+    scanf("%f", &price);
+
+    printf("Enter the discount percentage: ");
+    scanf("%f", &percentage);
+
+    finalPrice = cal_discount(price, percentage);
+    printf("The discounted price is: %.2f\n", finalPrice);
+
+    return 0;
 }
