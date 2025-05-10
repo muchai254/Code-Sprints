f1855befbc18840fb701fcc651d231db6d24c94e
CodieMisiani <nimrodmisiani1@gmail.com>
2025-05-10 12:57:14 +0300

    feat: add function to count vowels in a given word

--- problems/easy/vowels/vowels.js
+++ problems/easy/vowels/vowels.js
@@ -1,6 +1,15 @@
-
-function count_vowels(word) {
-let vowels =0;
-    //write your code here
+function getvowels(word) {
+    let vowels = 0;
+    const vowelSet = 'aeiouAEIOU'; // Including uppercase for safety
+  
+    for (let i = 0; i < word.length; i++) {
+      if (vowelSet.includes(word[i])) {
+        vowels += 1;
+      }
+    }
+  
     return vowels;
-}
+  }
+  console.log(getvowels("Celebration"));
+  module.exports = getvowels;
+  
